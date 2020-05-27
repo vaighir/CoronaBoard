b1d7364646c18940b893e0321dff70df20b95475
Wiktor Sierpinski <wiktor.sierpinski@de.clara.net>
2020-05-27 15:37:29 +0200

    Fix front-end password verification

--- src/flask_app/flaskr/static/scripts/auth.js
+++ src/flask_app/flaskr/static/scripts/auth.js
@@ -1,22 +1,23 @@
-$("form").submit(function(e){
-        e.preventDefault();
-        alert('it is working!');
-    });
-
 $(document).ready(function(){
-    $('sfsaf').click(function(){
-        var password = $('#password').val();
-        alert(password);
-        var password2 = $('#password2').val();
-        if(password != password2){
-            alert('the passwords didn\'t match!');
-        }
-
-    });
+    $("#error_box").hide();
 });
 
 $(document).ready(function(){
-  $("h1").click(function(){
-    $(this).hide();
+    $("form").submit(function(e){
+        var password = $('#password').val();
+        var password2 = $('#password2').val();
+
+        if(password.length < 8){
+            e.preventDefault();
+            $("#error_box").html("Password has to be longer than 7 characters");
+        }
+
+        if(password != password2){
+            e.preventDefault();
+            $("#error_box").html("Passwords have to match");
+        }
+
+        $("#error_box").show();
+
   });
 });
